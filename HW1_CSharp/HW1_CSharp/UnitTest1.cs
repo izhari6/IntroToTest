82faf34008076923fe969f7e8c116ccbbe1ee81b
Izhar Ananiev <izhar_ananiev@hotmail.com>
2017-11-29 19:30:18 +0200

    More tests

--- HW1_CSharp/HW1_CSharp/UnitTest1.cs
+++ HW1_CSharp/HW1_CSharp/UnitTest1.cs
@@ -86,5 +86,32 @@ namespace HW1_CSharp
             HeapSortClass.HeapSort(arr);
             CollectionAssert.AreEqual(expected, arr1, "Arrays are different");
         }
+
+        [TestMethod]
+        public void TestSeveralSameArr() //several numbers are the same, expected to be the same after sort
+        {
+            int[] arr = new int[] { 4, 2, 2, 1 };
+            int[] expected = new int[] { 1, 2, 2, 4 };
+            HeapSortClass.HeapSort(arr);
+            CollectionAssert.AreEqual(expected, arr);
+        }
+
+        [TestMethod]
+        public void TestMaxIntArr() //Int32.MaxValue in the arr, expected to be the same after sort
+        {
+            int[] arr = new int[] { Int32.MaxValue,4, 2, 1 };
+            int[] expected = new int[] { 1, 2, 4, Int32.MaxValue };
+            HeapSortClass.HeapSort(arr);
+            CollectionAssert.AreEqual(expected, arr);
+        }
+
+        [TestMethod]
+        public void TestMinIntArr() //Int32.MinValue in the arr, expected to be the same after sort
+        {
+            int[] arr = new int[] { Int32.MinValue, 4, 2, 1 };
+            int[] expected = new int[] { Int32.MinValue, 1, 2, 4 };
+            HeapSortClass.HeapSort(arr);
+            CollectionAssert.AreEqual(expected, arr);
+        }
     }
 }
